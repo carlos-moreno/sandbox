27dbcc2d6ba26fc2e5d7519c32410317ce3789c1
carlos-moreno <omorenodovale@gmail.com>
2024-01-06 02:15:56 +0000

    refactor: Adjustment to improve for performance and leave cleaner code

--- sandbox.py
+++ sandbox.py
@@ -1,13 +1,9 @@
-arquivos = ['xpto_20240105_001_Z01.txt', 'xptz_20240105_001_Z01.txt', 'xxts_20240105_001_Z02.txt']
+arquivos = ['xpto_20240105_001_Z01.txt', 'xptz_20240105_001_Z01.txt', 'xxts_20240105_001_Z02.txt', 'xxts_20240105_001_Z03.csv']
 
 sublistas = {}
 
 for arquivo in arquivos:
     zona = arquivo.split('_')[-1].split('.')[0]
-
-    if zona in sublistas:
-        sublistas[zona].append(arquivo)
-    else:
-        sublistas[zona] = [arquivo]
+    sublistas.setdefault(zona, []).append(arquivo)
 
 print(sublistas)
